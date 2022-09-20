3b68db732855f98e877fd50606858203d77a3a15
Slown61 <51879701+Slown61@users.noreply.github.com>
2022-09-20 23:52:00 +0200

    Update linux-installer.sh
    ## NON TESTER
    
    Ajout de la confirmation de l'installation de wine.
    
    Mise en place de l'interaction avec l'utilisateur.

--- linux-installer.sh
+++ linux-installer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-sudo apt install wine && sudo snap install spotify discord sosumi
+echo "Mise à jour du systeme et installation aptitude"
+echo "######################"
+sudo apt update && sudo apt install -y aptitude && sudo aptitude -y upgrade
+echo "Voulez-vous installer wine [o / n] ?
+read rep
+if ["$rep"="o"];
+then
+echo "Installation de wine"
+sudo aptitude -y install wine
+echo "#######################"
+echo "Installation de Spotify, discord et sosumi"
+sudo snap install spotify discord sosumi
+else
+echo "Wine ne sera pas installer"
+echo "############################"
+echo "Installation de Spotify, discord et sosumi"
+sudo snap install spotify discord sosumi
 # installer uniquement wine si vous etes pas sur Zorin OS
 # installer uniquement sosumi si vous avez les paquets snap faites pas ca si vous etes sur debian ou Linux Mint
