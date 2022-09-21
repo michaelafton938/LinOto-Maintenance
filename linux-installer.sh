2318c291cdb280958319e939af40882ff47bc24d
Slown61 <51879701+Slown61@users.noreply.github.com>
2022-09-21 20:56:05 +0200

    Update linux-installer.sh
    ## Demande à l'utilisateur s'il faut installer wine.
    ## Installation du paquet aprtitude pour une meilleure gestion des dépôts et dépendances.
    ## Mise en forme de l'affichage.

--- linux-installer.sh
+++ linux-installer.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-sudo apt install wine && sudo snap install spotify discord sosumi
+echo "######################"
+echo "Mise à jour du systeme et installation aptitude"
+echo "######################"
+sudo apt update && sudo apt install -y aptitude && sudo aptitude -y upgrade
+echo "Voulez-vous installer wine [o/n] ?"
+read rep
+if [ "$rep" == "o" -o "$rep" == "y" ]
+then
+echo "Installation de wine"
+sudo aptitude -y install wine
+echo "######################"
+echo "Installation de Spotify, discord et sosumi"
+sudo snap install spotify discord sosumi
+else
+echo "Wine ne sera pas installer"
+echo "######################"
+echo "Installation de Spotify, discord et sosumi"
+sudo snap install spotify discord sosumi
+fi
 # installer uniquement wine si vous etes pas sur Zorin OS
 # installer uniquement sosumi si vous avez les paquets snap faites pas ca si vous etes sur debian ou Linux Mint
