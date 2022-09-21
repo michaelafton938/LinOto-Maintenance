3790cf4ba2bd7d5ad0df24a13a30d4d6635470eb
Alexis <110925334+Xx-R3dL3ad3r-xX@users.noreply.github.com>
2022-09-21 17:09:56 +0200

    Delete linux-installer.sh

--- linux-installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-sudo apt install wine && sudo snap install spotify discord sosumi
-# installer uniquement wine si vous etes pas sur Zorin OS
-# installer uniquement sosumi si vous avez les paquets snap faites pas ca si vous etes sur debian ou Linux Mint
