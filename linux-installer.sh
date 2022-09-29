33625c0dc7da5e82fd806ad96e37b3f4330296d0
Slown61 <51879701+Slown61@users.noreply.github.com>
2022-09-29 18:26:52 +0200

    Update linux-installer.sh
    Correction de la syntaxe bash.
    Redéclaration de la variable rep.
    Ajout d'indications à l'utilisateur.

--- linux-installer.sh
+++ linux-installer.sh
@@ -10,12 +10,26 @@ if [ "$rep" == "o" -o "$rep" == "y" ]
 then
 echo "Installation de wine"
 sudo aptitude -y install wine
-echo "######################"
-echo "Installation de Spotify, discord et sosumi"
-sudo snap install spotify discord sosumi
 else
 echo "Wine ne sera pas installer"
 echo "######################"
+fi
+echo "Voulez-vous installer snapd et snap [o/n] ?"
+read rep
+if [ "$rep" == "o" -o "$rep" == "y" ]
+then
+echo "Installationde snap"
+sudo aptitude -y install snapd snap
+else
+echo "Snap ne sera pas installer"
+echo "######################"
+fi
+echo "Voulez-vous installer Discord,Spotify,Sosumi"
+read rep
+if [ "$rep" == "o" -o "$rep" == "y" ]
+then
 echo "Installation de Spotify, discord et sosumi"
 sudo snap install spotify discord sosumi
+else
+echo "Spotify, discord et sosumi ne seront pas installer"
 fi
